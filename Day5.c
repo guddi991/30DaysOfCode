5673730d33c2b391bc227eea6430fa15716925de
guddi yadav <69110026+guddi991@users.noreply.github.com>
2021-12-23 18:52:21 +0530

    Create Day5.c

---
+++ Day5.c
@@ -0,0 +1,21 @@
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i, n,result = 0;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++){
+        result = n*i;
+        printf("%d x %d = %d\n",n,i,result);
+        
+    }
+}
